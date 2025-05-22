9a7aaee007f2b4f32c20ec829228f9fdc47e98b6
IrvinTM <irvintm23@gmail.com>
2025-05-21 21:52:40 -0600

    working

--- app/src/main/java/com/example/dam/MainActivity.java
+++ app/src/main/java/com/example/dam/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.dam;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -13,6 +22,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextUsername = findViewById(R.id.editTextText);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
+        Button salirButton = findViewById(R.id.salir_button);
+
+        salirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+        editTextUsername = findViewById(R.id.editTextText);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+
+        Button ingresarButton = findViewById(R.id.ingresar_button);
+        ingresarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verificarCredenciales();
+            }
+        });
+
     }
 
     @Override
@@ -42,5 +78,33 @@ public class MainActivity extends AppCompatActivity {
             this.finishAffinity();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void verificarCredenciales() {
+        String inputUsername = editTextUsername.getText().toString();
+        String inputPassword = editTextPassword.getText().toString();
+
+        if (inputUsername.length() < 3){
+            Toast.makeText(this, "El usuario tiene que tener mas de 3 caracteres", Toast.LENGTH_SHORT).show();
+        }else if(inputPassword.length() < 5){
+            Toast.makeText(this, "El password tiene que tener mas de 5 caracteres", Toast.LENGTH_SHORT).show();
+        }else{
+            SharedPreferences sharedPref = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+            String storedUsername = sharedPref.getString(inputUsername, null);
+            String storedPassword = sharedPref.getString(inputPassword, null);
+
+            if (storedUsername != null && storedPassword != null) {
+                if (storedUsername.equals(inputUsername) && storedPassword.equals(inputPassword)) {
+                    Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish(); // Opcional: cerrar MainActivity
+                } else {
+                    Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "No hay usuario registrado con estas credenciales. Por favor, regístrese primero.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dam/RegistrarActivity.java
+++ app/src/main/java/com/example/dam/RegistrarActivity.java
@@ -1,6 +1,11 @@
 package com.example.dam;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistrarActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private  EditText editTextEmail;
+    private EditText editTextConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,63 @@ public class RegistrarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
+        editTextConfirmPassword = findViewById(R.id.editTextPasswordConfirm);
+
+    }
+
+    public void goBack(View view){
+        getOnBackPressedDispatcher().onBackPressed();
+    }
+
+    public void guardarDatos(View view) {
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String passworConfirm = editTextConfirmPassword.getText().toString();
+
+        if (!username.isEmpty() && !password.isEmpty()) {
+            if(username.length() < 3){
+                Toast.makeText(this, "El usuario tiene que tener mas de 3 caracteres", Toast.LENGTH_SHORT).show();
+            }else{
+                if(!isValidEmail(email)){
+                    Toast.makeText(this, "El email no es un email valido", Toast.LENGTH_SHORT).show();
+                }else {
+                    if( !password.equals(passworConfirm)){
+                        Toast.makeText(this, "Los passwords no coinciden", Toast.LENGTH_SHORT).show();
+                    }else{
+                        if (password.length() < 5){
+                            Toast.makeText(this, "El password debe de tener mas de 5 caracteres", Toast.LENGTH_SHORT).show();
+                        }else{
+                            SharedPreferences sharedPref = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPref.edit();
+                            editor.putString(username, username);
+                            editor.putString(password, password);
+                            editor.apply();
+
+                            Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+                            editTextUsername.setText("");
+                            editTextEmail.setText("");
+                            editTextPassword.setText("");
+                            editTextConfirmPassword.setText("");
+                        }
+
+                    }
+
+                }
+
+            }
+        } else {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static boolean isValidEmail(String email) {
+        String regex = "^[\\w\\.-]+@[\\w\\.-]+\\.[a-zA-Z]{2,}$";
+        return email != null && email.matches(regex);
     }
 }
+
